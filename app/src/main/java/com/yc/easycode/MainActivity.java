f9992be25edff5a759a70a118206eda52b96ab86
yc <563919364@qq.com>
2018-05-21 16:12:11 +0800

    测试提交1

--- app/src/main/java/com/yc/easycode/MainActivity.java
+++ app/src/main/java/com/yc/easycode/MainActivity.java
@@ -3,11 +3,14 @@ package com.yc.easycode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.yc.yclibrary.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Test.HelloWorld();
     }
 }
