fde778c5290f2a34af9bdbc086887dc489c0c3e9
yc <563919364@qq.com>
2018-05-23 09:35:09 +0800

    1.测试

--- yclibrary/src/main/java/com/yc/yclibrary/Test.java
+++ yclibrary/src/main/java/com/yc/yclibrary/Test.java
@@ -11,4 +11,7 @@ public class Test {
     public static void HelloWorld(){
         Log.e("Hello","HelloWorld");
     }
+    public static void HelloWorld2(){
+        Log.e("Hello","HelloWorld");
+    }
 }
