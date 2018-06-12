afa529453db19a474b27100460a932c2706eb719
yc <563919364@qq.com>
2018-06-12 15:55:16 +0800

    1.TEST

--- yclibrary/src/main/java/com/yc/yclibrary/Test.java
+++ yclibrary/src/main/java/com/yc/yclibrary/Test.java
@@ -8,10 +8,10 @@ import android.widget.Button;
  */
 
 public class Test {
-    public static void HelloWorld(){
-        Log.e("Hello","HelloWorld");
-    }
-    public static void HelloWorld2(){
-        Log.e("Hello","HelloWorld");
-    }
+//    public static void HelloWorld(){
+//        Log.e("Hello","HelloWorld");
+//    }
+//    public static void HelloWorld2(){
+//        Log.e("Hello","HelloWorld");
+//    }
 }
